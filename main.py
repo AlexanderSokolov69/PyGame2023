e88915b165a60b58086524ef817e934e4e2be8f0
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2023-11-21 11:18:36 +0300

    First Commit

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
 def main():
-    print('This is my First REPO!')
-    print('Second string')
-    
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+
 if __name__ == '__main__':
     main()
     
